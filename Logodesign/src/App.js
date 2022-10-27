81383a4cdc23d679e70fd295e1671a805ae749d1
siva-83 <98263290+siva-83@users.noreply.github.com>
2022-10-27 17:14:19 +0530

    added files through upload

---
+++ Logodesign/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Scorecard} from "./components/scorecard"
+function App() {
+  return (
+    <div>
+       {/* <div>
+        <h1>1</h1>
+        <h1>1</h1>
+        </div> */}
+      <Scorecard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Logodesign/src/components/scorecard/index.js
@@ -0,0 +1,17 @@
+import {useState,useEffect} from "react"
+import "./index.css"
+
+export function Scorecard(){
+    return(
+       <div className="outside">
+        <div className="main">
+            <div className="final">
+        <h1 className="heading">HTML&CSS</h1>
+        <p className="para">Design and build websites</p>
+        </div>
+
+        </div>
+        </div>
+   
+    )
+}
